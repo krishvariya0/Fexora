b91afa51be7d99bfc4e6a25d4f84d4dea0efe1c3
krishvariya0 <variyakrish0@gmail.com>
2025-12-04 21:15:10 +0530

    Fix Router import + build issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import './index.css';
-import router from './router.jsx';
+import "./index.css";
+import router from "./router.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} >
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        closeButton={false}
-        hideProgressBar={false}
-        newestOnTop={true}
-        pauseOnHover={true}
-        draggable={true}
-        style={{ zIndex: 999999 }}
-      />
-    </RouterProvider >
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      closeButton={false}
+      hideProgressBar={false}
+      newestOnTop={true}
+      pauseOnHover={true}
+      draggable={true}
+      style={{ zIndex: 999999 }}
+    />
+  </StrictMode>
+);
